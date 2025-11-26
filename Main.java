ce114e664ced4e398c41f4b5992a62b6523f13ce
rose yau <2357046517@qq.com>
2025-11-26 18:28:28 +0800

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public static void main(String[] args){
-    int a=10,b=15;
-    System.out.println("b>a is "+(b>a));
-    Scanner sc=new Scanner(System.in);
-    int x= sc.nextInt();
-    System.out.println(x);
-
-}
